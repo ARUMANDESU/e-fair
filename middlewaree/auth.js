38272d6e2f86453f670f17a912c7c196ee883844
Arumandesu <zh.arumandes@gmail.com>
2022-05-03 11:13:12 +0600

    user profile

---
+++ middlewaree/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+const {secret} = require('../config')
+const User = require("../modules/User")
+const Role = require("../modules/Role")
+
+module.exports = function () {
+    return async function (req, res, next) {
+        if (req.method === "OPTIONS") {
+            next()
+        }
+
+        try {
+            const token=req.cookies.auth.split(' ')[1]
+            if (!token) {
+                return res.status(403).json({message: "User not authorized"})
+            }
+            const user = jwt.verify(token, secret)
+            res.user= await User.findById(user.id)
+            next();
+        } catch (e) {
+            console.log(e)
+            return res.status(403).json({message: "User not authorized"})
+        }
+    }
+};
--- routers/authRouter.js
+++ routers/authRouter.js
@@ -6,62 +6,21 @@ const controller =require("./authController")
 const {check, checkSchema} = require("express-validator")
 const authMiddleware = require("../middlewaree/authMiddleware")
 const roleMiddleware= require("../middlewaree/roleMiddleware")
+const auth =require("../middlewaree/auth")
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser')
 const jwt = require('jsonwebtoken')
 const {secret} = require('../config')
 
-const  findUser=async (req,res)=>{
-    try{
-        const token=req.cookies.auth.split(' ')[1]
-        if (!token) {
-            return res.status(403).json({message: "User not authorized"})
-        }
-
-        const u =  jwt.verify(token, secret)
-        // console.log(u.id)
-
-        return user1
-    }
-    catch (e){
-
-    }
-    return null
-}
-// const setUser1=async (user)=>{
-//     user1.email=user.email;
-//     user1.username=user1.username;
-//     user1.avatarUrl=user1.avatarUrl;
-//     user1.description=user1.description;
-//     user1.fullname=user1.fullname
-//     user1.phoneNumber=user1.phoneNumber
-//     user1.address=user1.address
-//     user1.twitterUrl=user1.twitterUrl
-//     user1.instagramUrl=user1.instagramUrl
-//     user1.facebookUrl=user1.facebookUrl
-// }
 router.use(cookieParser())
 
 router.use(bodyParser.json())
 router.use(bodyParser.urlencoded({ extended: true }));
 
-let user;
-let auth=user?true:false;
-// let user1={
-//     email:"",
-//     username:"",
-//     avatarUrl:"",
-//     description:"",
-//     fullname:"",
-//     phoneNumber:"",
-//     address:"",
-//     twitterUrl:"",
-//     instagramUrl:"",
-//     facebookUrl:"",
-// }
+
 router
     .get('/login', (req, res) => {
-        auth=user?true:false;
+    //     auth=user?true:false;
         console.log(auth)
     res.render("login",{auth:auth,})
 
@@ -81,43 +40,37 @@ router
     .get("/users" , roleMiddleware(["ADMIN"]),controller.getUsers);
 
 router.get('/', (req, res) => {
-    user= findUser(req,res)
-    auth=user?true:false;
+
     res.redirect("/home");
 })
 
 router.get('/home', (req, res) => {
-    user= findUser(req,res)
-    auth=user?true:false;
+
     res.render("index",{auth:auth,})
 })
 router.get('/about', (req, res) => {
-    user= findUser(req,res)
-    auth=user?true:false;
+
     res.render('aboutUs',{auth:auth,})
 })
 router.get('/catalog', (req, res) => {
-    user= findUser(req,res)
-    auth=user?true:false;
+
     res.render("catalog",{auth:auth,})
 })
-router.get('/user/profile/:id',(req, res) => {
-    User.findById(findUser(req,res).id,(err,results)=>{
-        res.render("personalArea",{
-            auth:auth,
-            edit:false,
-            pavatarUrl:results,
-            pusername:results,
-            pfullname:"ok",
-            pphoneNumber:user,
-            paddress:user,
-            pemail:user,
-            pdescription:user,
-            ptwitterUrl:user,
-            pinstagramUrl:user,
-            pfacebookUrl:user,
-            pcomments:[],
-        })
+router.get('/user/profile/:id',auth(),async (req, res) => {
+    res.render("personalArea",{
+        auth:res.user,
+        edit:false,
+        pavatarUrl:res.user.avatarUrl,
+        pusername:res.user.username,
+        pfullname:res.user.fullname,
+        pphoneNumber:res.user.phoneNumber,
+        paddress:res.user.address,
+        pemail:res.user.email,
+        pdescription:res.user.description,
+        ptwitterUrl:res.user.twitterUrl,
+        pinstagramUrl:res.user.instagramUrl,
+        pfacebookUrl:res.user.facebookUrl,
+        pcomments:[],
     })
 
 
@@ -125,23 +78,20 @@ router.get('/user/profile/:id',(req, res) => {
 
 })
 router
-    .get('/user/edit',(req, res) => {
-        user= findUser(req,res)
-        auth=user?true:false;
-
+    .get('/user/edit',auth(),(req, res) => {
         res.render("personalArea",{
-            auth:auth,
+            auth:res.user,
             edit:true,
-            pavatarUrl:user.avatarUrl,
-            pusername:user.username,
-            pfullname:user.fullname,
-            pphoneNumber:user.phoneNumber,
-            paddress:user.address,
-            pemail:user.email,
-            pdescription:user.description,
-            ptwitterUrl:user.twitterUrl,
-            pinstagramUrl:user.instagramUrl,
-            pfacebookUrl:user.facebookUrl,
+            pavatarUrl:res.user.avatarUrl,
+            pusername:res.user.username,
+            pfullname:res.user.fullname,
+            pphoneNumber:res.user.phoneNumber,
+            paddress:res.user.address,
+            pemail:res.user.email,
+            pdescription:res.user.description,
+            ptwitterUrl:res.user.twitterUrl,
+            pinstagramUrl:res.user.instagramUrl,
+            pfacebookUrl:res.user.facebookUrl,
             pcomments:[],
         })
     })
