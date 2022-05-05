1458b6f019c5feb364e8fb9cf37660c9459273ec
Arumandesu <zh.arumandes@gmail.com>
2022-05-05 18:39:44 +0600

    old avatars delete

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -58,8 +58,10 @@ class profileController{
                 return res.status(403).json({message: "User not authorized"})
             }
             const u = jwt.verify(token, process.env.secret)
-
-            const result = await cloudinary.uploader.upload(req.file.path,{folder:"avatars",transformation: [{width: 150, height: 150, crop: "thumb"}]});
+            const oldAvatar=res.user.avatarUrl.split("/")
+            const public_id=`${oldAvatar[7]+"/"+oldAvatar[8].slice(0,20)}`
+            const deleteResult= await  cloudinary.uploader.destroy(`${public_id}`);
+            const result = await cloudinary.uploader.upload(req.file.path,{folder:"avatars",transformation: [{width: 250, height: 250, crop: "thumb"}]});
             const user = await User.updateOne({_id:u.id},{
                 avatarUrl:result.secure_url,
                 phoneNumber:req.body.iphonenumber,
