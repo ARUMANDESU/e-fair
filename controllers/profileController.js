f724cb7a0341fe65fb8ad40e6f5b14ca9160f6ca
Arumandesu <zh.arumandes@gmail.com>
2022-06-15 15:39:08 +0600

    .

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -112,14 +112,23 @@ class profileController{
             const u = jwt.verify(token, process.env.secret)
             const oldAvatar=res.user.avatarUrl.split("/")
             console.log(oldAvatar)
-            if(oldAvatar!="https://res.cloudinary.com/nezz/image/upload/v1651755541/avatars/ecce-homo_j36lz7.jpg"){
-                const public_id=`${oldAvatar[7]+"/"+oldAvatar[8].slice(0,20)}`
-                const deleteResult= await  cloudinary.uploader.destroy(`${public_id}`);
+            if(oldAvatar[2]=='lh3.googleusercontent.com'){
+                const result = await cloudinary.uploader.upload(req.file.path,{folder:"avatars",transformation: [{width: 250, height: 250, crop: "thumb"}]});
+                const user = await User.updateOne({_id:u.id},{
+                    avatarUrl:result.secure_url,
+                })
             }
-            const result = await cloudinary.uploader.upload(req.file.path,{folder:"avatars",transformation: [{width: 250, height: 250, crop: "thumb"}]});
-            const user = await User.updateOne({_id:u.id},{
-                avatarUrl:result.secure_url,
-            })
+            else{
+                if(oldAvatar!="https://res.cloudinary.com/nezz/image/upload/v1651755541/avatars/ecce-homo_j36lz7.jpg"){
+                    const public_id=`${oldAvatar[7]+"/"+oldAvatar[8].slice(0,20)}`
+                    const deleteResult= await  cloudinary.uploader.destroy(`${public_id}`);
+                }
+                const result = await cloudinary.uploader.upload(req.file.path,{folder:"avatars",transformation: [{width: 250, height: 250, crop: "thumb"}]});
+                const user = await User.updateOne({_id:u.id},{
+                    avatarUrl:result.secure_url,
+                })
+            }
+
             res.render("message",{auth:res.user,user:[],message:"changed",timeout:100,where:`/user/profile/${res.user.username}`})
         }
         catch (e) {
